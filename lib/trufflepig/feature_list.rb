1955c3d02c8df0cea4969eb2ee8a1552c3c03d6b
Sebastian Kippe <sebastian@kip.pe>
2012-03-25 01:52:55 +0100

    Remove comments

--- lib/trufflepig/feature_list.rb
+++ lib/trufflepig/feature_list.rb
@@ -7,19 +7,14 @@ module Trufflepig
     end
 
     def self.build
-      # read data from caniuse JSON
-      caniuse = JSON.parse File.read("#{File.dirname(__FILE__)}/../../data/caniuse.json")
-
-      # Read data from patterns JSON
+      caniuse  = JSON.parse File.read("#{File.dirname(__FILE__)}/../../data/caniuse.json")
       patterns = JSON.parse File.read("#{File.dirname(__FILE__)}/../../data/patterns.json")
-
-      # Merge data
       features = caniuse["data"]
+
       patterns.keys.each do |key|
         features[key].merge!({"detection_pattern" => patterns[key]})
       end
 
-      # Write features to file
       of = File.open "#{File.dirname(__FILE__)}/../../data/features.json", "w"
       of.syswrite caniuse["data"].to_json
       of.close
